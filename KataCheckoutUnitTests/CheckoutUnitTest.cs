3eb0cdcd098d08780fdbc47cac54493426b14387
Carlos Gonzalez Rodriguez <cgonzalezrodriguez@vistaprint.com>
2016-06-21 00:19:21 +0200

    Adding all the test for the totals from the Kata web page (all passing without changes)

--- KataCheckoutUnitTests/CheckoutUnitTest.cs
+++ KataCheckoutUnitTests/CheckoutUnitTest.cs
@@ -14,6 +14,7 @@ namespace KataCheckoutUnitTests
             var co = new Checkout(new List<IRule>
             {
                 new SetOfProductsRule("AAA", 130),
+                new SetOfProductsRule("BB", 45),
                 new SetOfProductsRule("A", 50),
                 new SetOfProductsRule("B", 30),
                 new SetOfProductsRule("C", 20),
@@ -63,6 +64,26 @@ namespace KataCheckoutUnitTests
         public void ThreeOfTheSameProduct()
         {
             Assert.AreEqual(130, Price("AAA"));
+        }
+
+        [Test]
+        public void TestTotalsFromKataWebPage()
+        {
+            Assert.AreEqual(0, Price(""));
+            Assert.AreEqual(50, Price("A"));
+            Assert.AreEqual(80, Price("AB"));
+            Assert.AreEqual(115, Price("CDBA"));
+
+            Assert.AreEqual(100, Price("AA"));
+            Assert.AreEqual(130, Price("AAA"));
+            Assert.AreEqual(180, Price("AAAA"));
+            Assert.AreEqual(230, Price("AAAAA"));
+            Assert.AreEqual(260, Price("AAAAAA"));
+
+            Assert.AreEqual(160, Price("AAAB"));
+            Assert.AreEqual(175, Price("AAABB"));
+            Assert.AreEqual(190, Price("AAABBD"));
+            Assert.AreEqual(190, Price("DABABA"));
         }
 
     }
