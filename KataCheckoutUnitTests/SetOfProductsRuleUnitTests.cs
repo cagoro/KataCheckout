409e487e08b6ef3dd58f01ade79456df43e6b503
Carlos Gonzalez Rodriguez <cgonzalezrodriguez@vistaprint.com>
2016-06-20 23:23:14 +0200

    Added a couple of more complex tests, they still pass. Not sure if I added too much logic with the first test (probably yes)

--- KataCheckoutUnitTests/SetOfProductsRuleUnitTests.cs
+++ KataCheckoutUnitTests/SetOfProductsRuleUnitTests.cs
@@ -8,13 +8,37 @@ namespace KataCheckoutUnitTests
     public class SetOfProductsRuleUnitTests
     {
         [Test]
-        public void SingleProductSingleRule()
+        public void SingleProduct_RuleAppliesToTheProduct_RuleApply()
         {
             var rule = new SetOfProductsRule("A", 1);
             var products = new List<char> { 'A'};
             Assert.AreEqual(1, rule.ConsumeProducts(products));
             Assert.AreEqual(0, products.Count);
         }
-        
+
+        [Test]
+        public void MultipleProducts_RuleForOneProduct_RulesApplies()
+        {
+            var rule = new SetOfProductsRule("A", 1);
+            var products = new List<char> { 'D','A','B','B'};
+            var expectedRemainingProduts = new List<char> {'D', 'B', 'B'};
+            Assert.AreEqual(1, rule.ConsumeProducts(products));
+            Assert.AreEqual(expectedRemainingProduts, products);
+        }
+
+        [Test]
+        public void MultipleProducts_RulesForMoreThanOneProductButNotAllProductsFound_RuleDoesNotApply()
+        {
+            var rule = new SetOfProductsRule("CAB", 1);
+            var products = new List<char> { 'A', 'B', 'B' };
+            var expectedRemainingProduts = new List<char> { 'A', 'B', 'B' };
+            Assert.AreEqual(0, rule.ConsumeProducts(products));
+            Assert.AreEqual(expectedRemainingProduts, products);
+        }
+
+
+
     }
+
+
 }
